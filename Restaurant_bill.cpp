3fc826501d5aac3925ff8a50c1d5d6099508dce7
Hhbhoot <hiteshbhoot938@gmail.com>
2023-08-09 19:35:55 +0530

    Restaurant bill

--- Restaurant_bill.cpp
+++ Restaurant_bill.cpp
@@ -19,7 +19,7 @@ public:
 
 int calc(int x)
 {
-   
+
   float discount;
   float sgst, cgst;
   float netbill;
@@ -29,22 +29,26 @@ int calc(int x)
   cgst = (x * 2.5) / 100;
   netbill = x - discount + sgst + cgst;
 
-  cout<<"............................"<<endl;
-  cout<<"Total bill.................."<<"\t"<< x <<endl;
-  cout <<"Discount..................."<<"\t"<< discount << endl;
-  cout <<"SGST......................."<<"\t"<<  sgst << endl;
-  cout <<"CGST......................."<<"\t"<<  cgst << endl;
-  cout<<"............................"<<endl;
-  cout <<"Net amount................."<<"\t"<< netbill << endl;
-  
+  cout << "............................" << endl;
+  cout << "Total bill.................."
+       << "\t" << x << endl;
+  cout << "Discount..................."
+       << "\t" << discount << endl;
+  cout << "SGST......................."
+       << "\t" << sgst << endl;
+  cout << "CGST......................."
+       << "\t" << cgst << endl;
+  cout << "............................" << endl;
+  cout << "Net amount................."
+       << "\t" << netbill << endl;
 }
 
 int main()
 {
   bill b;
   int ch;
-  int quantity[10]={0,0,0,0,0,0,0,0,0,0};
-  int n[10]={0,0,0,0,0,0,0,0,0,0};
+  int quantity[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int n[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   int price;
   int amount;
   int total = 0;
@@ -73,7 +77,7 @@ int main()
       cin >> n[0];
       amount = 20 * n[0];
       total += amount;
-      quantity[0]+=n[0];
+      quantity[0] += n[0];
       break;
 
     case 2:
@@ -81,7 +85,7 @@ int main()
       cin >> n[1];
       amount = 30 * n[1];
       total += amount;
-      quantity[1]+=n[1];
+      quantity[1] += n[1];
 
       break;
 
@@ -90,65 +94,65 @@ int main()
       cin >> n[2];
       amount = 30 * n[2];
       total += amount;
-      quantity[2]+=n[2];
+      quantity[2] += n[2];
       break;
 
     case 4:
       cout << "Enter quantity " << endl;
       cin >> n[3];
 
-      amount =  40 * n[3];
+      amount = 40 * n[3];
       total += amount;
-      quantity[3]+=n[3];
+      quantity[3] += n[3];
       break;
 
     case 5:
       cout << "Enter quantity " << endl;
       cin >> n[4];
-      amount =  30 * n[4];
+      amount = 30 * n[4];
       total += amount;
-      quantity[4]+=n[4];
+      quantity[4] += n[4];
 
       break;
 
     case 6:
       cout << "Enter quantity " << endl;
       cin >> n[5];
-      amount =  20 * quantity[5];
+      amount = 20 * quantity[5];
       total += amount;
-      quantity[5]+=n[5];
+      quantity[5] += n[5];
       break;
 
     case 7:
       cout << "Enter quantity " << endl;
       cin >> n[6];
-      amount =  30 * quantity[6];
+      amount = 30 * quantity[6];
       total += amount;
-      quantity[6]+=n[6];
+      quantity[6] += n[6];
       break;
 
     case 8:
       cout << "Enter quantity " << endl;
       cin >> n[7];
-      amount =  30 * n[7];
+      amount = 30 * n[7];
       total += amount;
-      quantity[7]+=n[7];
+      quantity[7] += n[7];
       break;
 
     case 9:
       cout << "Enter quantity " << endl;
       cin >> n[8];
-      amount =  50 * n[8];
+      amount = 50 * n[8];
       total += amount;
-      quantity[8]+=n[8];
+      quantity[8] += n[8];
       break;
 
     case 10:
       cout << "Enter quantity " << endl;
       cin >> n[9];
-      amount =  10 * n[9];
+      amount = 10 * n[9];
       total += amount;
-      quantity[9]+=n[9];
+      quantity[9] += n[9];
       break;
 
     case 11:
@@ -162,29 +166,65 @@ int main()
     }
 
   } while (ch != 11);
-   cout << "            Hi - FI               " << endl;
-   cout << "      Fast Food & Cold drinks  " << endl;
- cout<<"Item..........."<<"\t"<<"Qty"<<"\t"<<"Price"<<"\t"<<"total"<<endl;
-if(quantity[0]!=0)
- cout<<"1.panipuri....."<<"\t"<<quantity[0]<<"\t"<<"20"<<"\t"<<20 * quantity[0]<<endl;
- if(quantity[1]!=0)
- cout<<"2.vadapav......"<<"\t"<<quantity[1]<<"\t"<<"30"<<"\t"<<30 * quantity[1]<<endl;
- if(quantity[2]!=0)
- cout<<"3.Kachci dabeli"<<"\t"<<quantity[2]<<"\t"<<"20"<<"\t"<<20 * quantity[2]<<endl;
- if(quantity[3]!=0)
- cout<<"4.Khavsa......."<<"\t"<<quantity[3]<<"\t"<<"40"<<"\t"<<40 * quantity[3]<<endl;
- if(quantity[4]!=0)
- cout<<"5.Samosa......."<<"\t"<<quantity[4]<<"\t"<<"30"<<"\t"<<30 * quantity[4]<<endl;
- if(quantity[5]!=0)
- cout<<"6.Ghughra......"<<"\t"<<quantity[5]<<"\t"<<"20 "<<"\t"<<20 * quantity[5]<<endl;
- if(quantity[6]!=0)
- cout<<"7.Fafdi Gathiya"<<"\t"<<quantity[6]<<"\t"<<"30"<<"\t"<<30 * quantity[6]<<endl;
- if(quantity[7]!=0)
- cout<<"8.Chat Papdi..."<<"\t"<<quantity[7]<<"\t"<<"30"<<"\t"<<30 * quantity[7]<<endl;
- if(quantity[8]!=0)
- cout<<"9.Momos........"<<"\t"<<quantity[8]<<"\t"<<"50"<<"\t"<<50 * quantity[8]<<endl;
- if(quantity[9]!=0)
- cout<<"10.Colddrinks.."<<"\t"<<quantity[9]<<"\t"<<"10"<<"\t"<<10 * quantity[9]<<endl;
+  cout << "            Hi - FI               " << endl;
+  cout << "      Fast Food & Cold drinks  " << endl;
+  cout << "Item..........."
+       << "\t"
+       << "Qty"
+       << "\t"
+       << "Price"
+       << "\t"
+       << "total" << endl;
+  if (quantity[0] != 0)
+    cout << "1.panipuri....."
+         << "\t" << quantity[0] << "\t"
+         << "20"
+         << "\t" << 20 * quantity[0] << endl;
+  if (quantity[1] != 0)
+    cout << "2.vadapav......"
+         << "\t" << quantity[1] << "\t"
+         << "30"
+         << "\t" << 30 * quantity[1] << endl;
+  if (quantity[2] != 0)
+    cout << "3.Kachci dabeli"
+         << "\t" << quantity[2] << "\t"
+         << "20"
+         << "\t" << 20 * quantity[2] << endl;
+  if (quantity[3] != 0)
+    cout << "4.Khavsa......."
+         << "\t" << quantity[3] << "\t"
+         << "40"
+         << "\t" << 40 * quantity[3] << endl;
+  if (quantity[4] != 0)
+    cout << "5.Samosa......."
+         << "\t" << quantity[4] << "\t"
+         << "30"
+         << "\t" << 30 * quantity[4] << endl;
+  if (quantity[5] != 0)
+    cout << "6.Ghughra......"
+         << "\t" << quantity[5] << "\t"
+         << "20 "
+         << "\t" << 20 * quantity[5] << endl;
+  if (quantity[6] != 0)
+    cout << "7.Fafdi Gathiya"
+         << "\t" << quantity[6] << "\t"
+         << "30"
+         << "\t" << 30 * quantity[6] << endl;
+  if (quantity[7] != 0)
+    cout << "8.Chat Papdi..."
+         << "\t" << quantity[7] << "\t"
+         << "30"
+         << "\t" << 30 * quantity[7] << endl;
+  if (quantity[8] != 0)
+    cout << "9.Momos........"
+         << "\t" << quantity[8] << "\t"
+         << "50"
+         << "\t" << 50 * quantity[8] << endl;
+  if (quantity[9] != 0)
+    cout << "10.Colddrinks.."
+         << "\t" << quantity[9] << "\t"
+         << "10"
+         << "\t" << 10 * quantity[9] << endl;
 
   calc(total);
 }
